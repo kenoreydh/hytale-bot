fba8813748f7cc4357bde3398ad64e0107153bf2
kenoreydh <112786674+kenoreydh@users.noreply.github.com>
2025-11-28 10:51:36 +0100

    Update config.ts relese 2

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,24 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
-
 export const config = {
     discord: {
         token: process.env.DISCORD_TOKEN || '',
         channelId: process.env.DISCORD_CHANNEL_ID || '',
     },
     twitter: {
-        nitterUrl: process.env.NITTER_URL || 'https://nitter.net',
+        // List of public RSS-Bridge instances to try (Round Robin)
+        rssBridgeUrls: [
+            'https://bridge01.rss-bridge.org',
+            'https://rss-bridge.flossboxin.org.in',
+            'https://rss-bridge.cheredeprince.net',
+            'https://rss-bridge.sans-nuage.fr',
+            'https://rss-bridge.lewd.tech',
+            'https://wtf.roflcopter.fr/rss-bridge',
+            'https://rss.nixnet.services',
+            'https://rss-bridge.ggc-project.de',
+            'https://rssbridge.bus-hit.me',
+            'https://feeds.proxeuse.com'
+        ],
     },
     // List of Twitter usernames to monitor
     monitoredAccounts: [
